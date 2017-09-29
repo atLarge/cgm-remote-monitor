b8d280b3013783436b4eaa394d6f303393f1b2af
atLarge <michael@seeleyfamily.ca>
2017-09-29 19:30:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@
 // Description: Basic web server to display data from Dexcom G4.  Requires a database that contains
 // the Dexcom SGV data.
 'use strict';
+require('sqreen');
 
 ///////////////////////////////////////////////////
 // DB Connection setup and utils
